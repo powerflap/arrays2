dab6a4c40c51118c5e9af46d956da74312ae455f
Женёк <muratov_yevgeniy@vk.com>
2024-12-09 17:12:35 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,27 @@ public class Main {
         System.out.println("Сумма трат за месяц составила " + sum + " рублей ");
         System.out.println("Задание № 2 ");
         int[] difference = {123123, 323, 33324, 44425, 76754};
-        int min = Arrays.stream(difference).min().getAsInt();
+        int min = difference[0];
+        for (int element:difference){
+            if(element<min) {
+                min=element;
+            }
+        }
         System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей");
-        int max = Arrays.stream(difference).max().getAsInt();
-        System.out.println("Максимальная сумма трат за неделю составила" + max + " рублей");
+        int max = difference[0];
+        for (int element: difference){
+            if(element>max) {
+                max=element;
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей");
         System.out.println("Задание № 3 ");
         int[] average = {12312, 34200, 56150, 42300};
         int value = 0;
         for (int element : average) {
-            value += element / average.length;
+            value += element;
         }
+        value = value/ average.length;
         System.out.println("Средняя сумма трат за месяц составила " + value + " рублей");
         System.out.println("Задание № 4 ");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
@@ -32,6 +43,14 @@ public class Main {
         }
 
         System.out.println(new String(reverseFullName));
+        var nums = new int[]{1, 4, -2, 3};
+        var minim = nums[0];
+        for (int element : nums) {
+            if (element < minim) {
+                minim = element;
+            }
+        }
+        System.out.println(minim);
     }
 }
 
